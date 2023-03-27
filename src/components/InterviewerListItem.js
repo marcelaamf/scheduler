7ba1987886cb5b54858ea7c575d5626acf8c2461
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-03-27 00:43:03 -0600

    Refactored DayList and InterviewerList. Add appintment files and storybook

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,26 +5,19 @@ import './InterviewerListItem.scss'
 export default function InterviewerListItem(props) {
   
 
-  let dayClass = classNames("interviewers__item", {
+  let interviewerClass = classNames("interviewers__item", {
     " interviewers__item--selected" : props.selected,
   });
 
-  const interviewer = () => {
-    if (props.selected) {
-    return <>{props.name} </>
-    } else {
-      return <></>
-    }
-  }
-
+  
   return (
-    <li className={dayClass} onClick={props.setInterviewer}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
       alt={props.name}
     />
-    <>{interviewer()}</>
+    <>{props.selected && props.name}</>
   </li>
   );
 }
